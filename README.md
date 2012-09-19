3b2fdd8c675b93181320531897ad26aaed4815c3
Leandro Pincini <leandropincini@gmail.com>
2012-09-18 22:40:36 -0300

    addding some survival files
    adding .gitignore .gitconfig and .vimrc

---
+++ README.md
@@ -0,0 +1,8 @@
+Hello,
+
+After lots of wasting time, I resolved to put this kind of stuff here.
+Here are the dot files I'm using in my macbook.
+
+Hope you enjoy!
+Thank you github.
+
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# owner
+REPO_OWNER="leandropincini"
+
+set -e
+
+echo "Installing dotfiles:"
+cp -v ./home/* ~/.dotfiles/
+
+for i in ~/.dotfiles/*; do
+	echo "Installing $(basename $i)..."
+	rm -f ~/.$(basename $i)
+	ln -s $i ~/.$(basename $i)
+done
+
