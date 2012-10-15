e9820d849de9b07b7d13b512e2ff8d377569bf32
Leandro Pincini <leandropincini@gmail.com>
2012-10-15 11:47:19 -0300

    fix permissions for install.sh, add some bashrc stuff
    fix created files by install.sh permissions
    add some bashrc basic features (aliases, exports)

--- install.sh
+++ install.sh
@@ -4,16 +4,19 @@ set -e
 
 echo ">>> Installing dotfiles:"
 cp -v ./home/* ~/.dotfiles/
+chmod 700 ~/.dotfiles/
 
 for i in ~/.dotfiles/*; do
 	echo "Installing $(basename $i)..."
 	rm -f ~/.$(basename $i)
+	chmod 600 $i
 	ln -s $i ~/.$(basename $i)
+	chmod 700 ~/.$(basename $i)
 done
 
 if [ $(uname) = "Darwin" ]; then
-echo ">>> Installing osx prefs"
-bash ./scripts/osx.sh
-echo "Done. Note that some of these changs require a logout/restart to take effect."
+	echo ">>> Installing osx prefs"
+	bash ./scripts/osx.sh
+	echo "Done. Note that some of these changs require a logout/restart to take effect."
 fi
 
