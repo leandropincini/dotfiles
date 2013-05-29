a7f21141d71f811e73ec986b563f080887500fc7
Leandro Pincini <leandropincini@gmail.com>
2013-05-28 22:33:53 -0300

    Added full python environment installation.

---
+++ scripts/python-environment.sh
@@ -0,0 +1,21 @@
+# install python with homebrew's up-to-date OpenSSl
+echo "Installing python with homebrew's up-to-date OpenSSL..."
+if [ ! -f /usr/local/bin/python ]; then
+	brew install python --with-brewed-openssl
+fi
+
+# install up-to-date pip's distribute
+echo "Installing distribute with pip..."
+
+# deactivating require virtualenv ;)
+PIP_REQUIRE_VIRTUALENV=""
+pip install --upgrade distribute
+
+# intall virtualenv
+echo "Installing virtualenv with pip..."
+pip install virtualenv
+
+# install mkvirtualenvwrapper
+echo "Installing mkvirtualenvwrapper with pip..."
+pip install virutalenvwrapper
+
