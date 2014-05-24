1032b264aa515e96f97326141edf1df9f817e68a
Leandro Pincini <leandropincini@gmail.com>
2014-05-24 13:44:03 -0300

    Added gradle install with homebrew.

--- scripts/osx/java-environment.sh
+++ scripts/osx/java-environment.sh
@@ -2,6 +2,10 @@
 echo "Installing maven with homebrew..."
 brew install maven
 
+# gradle
+echo "Installing gradle with homebrew..."
+brew install gradle
+
 # jenkins
 echo "Installing jenkins with homebrew..."
 brew install jenkins
