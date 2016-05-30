a04c9f6aaf26ea13d8bb575bb6beff07d182a2a8
Leandro Pincini <leandropincini@gmail.com>
2016-05-30 19:23:54 -0300

    Removed Jenkins brew's installation

--- scripts/osx/java-environment.sh
+++ scripts/osx/java-environment.sh
@@ -10,10 +10,6 @@ brew install gradle
 echo "Installing wildfly with homebrew..."
 brew install wildfly
 
-# jenkins
-echo "Installing jenkins with homebrew..."
-brew install jenkins
-
 # redis
 echo "Installing redis with homebrew..."
 brew install redis
