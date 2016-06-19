d5575d38e8a6490a975f26669fe416e0fef9f8d9
Leandro Pincini <leandropincini@gmail.com>
2016-06-19 18:51:22 -0300

    Added emacs dotfiles into install script

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ if [ $(uname) = "Darwin" ]; then
 	bash ./scripts/osx/osx.sh
 fi
 
+bash ./scripts/emacs.sh
 bash ./scripts/vim/vim.sh
 
 echo "Done. Note that some of these changes require a logout/restart to take
 effect."
-
