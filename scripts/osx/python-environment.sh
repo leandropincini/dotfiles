cac3bf2c06e24805a0b63d9d27d81ff087f99b3a
Leandro Pincini <leandropincini@gmail.com>
2016-08-15 15:20:25 -0300

    python updated

--- scripts/osx/python-environment.sh
+++ scripts/osx/python-environment.sh
@@ -1,24 +1,23 @@
-# install python with homebrew's up-to-date OpenSSl
-if [ ! -f /usr/local/bin/python ]; then
-	echo "Installing python with homebrew's up-to-date OpenSSL..."
-	brew install python --with-brewed-openssl
+# verify if python is already installed
+if [ -f /usr/local/bin/python ]; then
+    echo "Python is already installed..."
+
+    # install up-to-date pip's distribute
+    echo "Installing distribute with pip..."
+    # deactivating require virtualenv ;)
+    sudo pip install --upgrade distribute
+
+    # intall virtualenv
+    echo "Installing virtualenv with pip..."
+    sudo pip install --upgrade virtualenv
+
+    # install mkvirtualenvwrapper
+    echo "Installing mkvirtualenvwrapper with pip..."
+    sudo pip install --upgrade virtualenvwrapper
+
+    # install ipython
+    echo "Installing ipython with pip..."
+    sudo pip install --upgrade ipython
+else
+    echo "Install Python via official pacakge at https://www.python.org/downloads/"
 fi
-
-# install up-to-date pip's distribute
-echo "Installing distribute with pip..."
-
-# deactivating require virtualenv ;)
-sudo pip install --upgrade distribute
-
-# intall virtualenv
-echo "Installing virtualenv with pip..."
-sudo pip install --upgrade virtualenv
-
-# install mkvirtualenvwrapper
-echo "Installing mkvirtualenvwrapper with pip..."
-sudo pip install --upgrade virtualenvwrapper
-
-# install ipython
-echo "Installing ipython with pip..."
-sudo pip install --upgrade ipython
-
