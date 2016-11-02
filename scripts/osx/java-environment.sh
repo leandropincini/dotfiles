53b5f38c905c03b19d7da4986849d6f17a91cc7b
Leandro Pincini <leandropincini@gmail.com>
2016-11-02 14:52:30 -0200

    Moved to docker

--- scripts/osx/java-environment.sh
+++ scripts/osx/java-environment.sh
@@ -3,14 +3,14 @@ echo "Installing maven with homebrew..."
 brew install maven
 
 # gradle
-echo "Installing gradle with homebrew..."
-brew install gradle
+#echo "Installing gradle with homebrew..."
+#brew install gradle
 
 # widlfly
-echo "Installing wildfly with homebrew..."
-brew install wildfly
+#echo "Installing wildfly with homebrew..."
+#brew install wildfly-as
 
 # redis
-echo "Installing redis with homebrew..."
-brew install redis
+#echo "Installing redis with homebrew..."
+#brew install redis
 
