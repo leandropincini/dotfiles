c68c0b20cd6643302652346e0bdbac5c87bbf211
Leandro Pincini <leandropincini@gmail.com>
2017-03-15 17:09:52 -0300

    Added dracula-theme verification.

--- home/emacs.d/visual-configs/theme-configs.el
+++ home/emacs.d/visual-configs/theme-configs.el
@@ -1,7 +1,8 @@
 ;; theme-configs.el - emacs theme configs
 
-;; load dracula theme
-(load-theme 'dracula t)
+;; verify and load dracula theme
+(when (require 'dracula-theme nil t)
+  (load-theme 'dracula t))
 
 (provide 'theme-configs)
 ;; end of thtme-configs.el
