6ed6542cde6282e79e798892eb092935da4e90eb
Leandro Pincini <leandropincini@gmail.com>
2017-06-17 14:18:12 -0300

    Switched for zsh and oh-my-zsh

--- scripts/osx/osx.sh
+++ scripts/osx/osx.sh
@@ -174,8 +174,28 @@ echo "Installing p7zip with homebrew..."
 brew install p7zip
 
 # install bash-completion with homebrew
-echo "Installing bash-completion with homebrew..."
-brew install bash-completion
+#echo "Installing bash-completion with homebrew..."
+#brew install bash-completion
+
+# install zsh and zsh-completions with homebrew
+echo "Installing zsh and zsh-completions with homebrew..."
+brew install zsh zsh-completions
+
+# install oh my zsh
+echo "Installing oh my zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install oh my zsh dracula theme
+echo "Installing oh my zsh dracula-theme..."
+if ![ -d ~/Projects ]; then
+    mkdir ~/Projects
+fi
+if ![ -d ~/Projects/dracula-theme ]; then
+    mkdir ~/Projects/dracula-theme
+fi
+cd ~/Projects/dracula-theme
+git clone https://github.com/dracula/zsh.git
+ln -s ~/Projects/dracula-theme/zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
 
 # replace vim with macvim
 #echo "Replacing vim with macvim (with homebrew)..."
