a9627d08fd7d165048142c0b31006fff593e8a2e
Leandro Pincini <leandropincini@gmail.com>
2017-12-18 14:38:41 -0200

    Fixed after zsh installation problem.

--- install.sh
+++ install.sh
@@ -6,12 +6,13 @@ bash ./scripts/dotfiles.sh
 
 bash ./scripts/ssh.sh
 
+bash ./scripts/emacs.sh
+
+bash ./scripts/vim/vim.sh
+
 if [ $(uname) = "Darwin" ]; then
 	bash ./scripts/osx/osx.sh
 fi
-
-bash ./scripts/emacs.sh
-bash ./scripts/vim/vim.sh
 
 echo "Done. Note that some of these changes require a logout/restart to take
 effect."
