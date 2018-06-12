3b4da1e91448065f94f6f4db28fd0f3f68c67c33
Leandro Pincini <leandropincini@gmail.com>
2018-06-12 13:51:07 -0300

    Emacs dockerfile and docker-compose modes config

--- home/emacs.d/editor-configs/docker-compose-mode-configs.el
+++ home/emacs.d/editor-configs/docker-compose-mode-configs.el
@@ -1,3 +1,8 @@
+;; docker-compose-mode-configs.el - emacs docker-compose-mode editor configs
+
 (require 'docker-compose-mode)
 
+(add-to-list 'auto-mode-alist '("\\docker-compose.yml\\'" . docker-compose-mode))
+
 (provide 'docker-compose-mode-configs)
+;; end of docker-compose-mode-configs.el
