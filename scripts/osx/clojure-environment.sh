d6cdf8873d3503d90872a08e10be6d8920963280
Leandro Pincini <leandropincini@gmail.com>
2020-02-26 18:58:15 -0300

    add aspell to clojure-env script

--- scripts/osx/clojure-environment.sh
+++ scripts/osx/clojure-environment.sh
@@ -9,3 +9,7 @@ brew install leiningen
 # clojure-lsp
 echo "Installing clojure-lsp with homebrew..."
 brew install clojure-lsp
+
+# aspell
+echo "Installing aspell with homebrew..."
+brew install aspell
