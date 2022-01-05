33f4d5c5bac0f21c1c4e377dd52c4462205379e2
Leandro Pincini <leandropincini@gmail.com>
2022-01-05 19:44:01 -0300

    Auto start X with archlinux
    - Add auto start X on .zprofile
    - Fix install-archlinux.sh to remove the .zshrc link if it already
      exists

--- install-archlinux.sh
+++ install-archlinux.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 env RCRC=/dev/null
 rcup -t archlinux -t global -x README.md -x docs -x home -x install.sh -x scripts -x toolbox -x .git -x .gitconfig -x .gitignore -x .editorconfig -x rcstart-archlinux -x rcstart-macos -B valholl -q
-ln -s ~/.config/zsh/zshrc ~/.config/zsh/.zshrc
+if [ -f ~/.config/zsh/.zshrc ]; then
+	rm ~/.config/zsh/.zshrc
+fi
+ln -sv ~/.config/zsh/zshrc ~/.config/zsh/.zshrc
