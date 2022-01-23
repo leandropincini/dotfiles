e96fa444abc8959140f382f40fe79bdcfd6fbfec
Leandro Pincini <leandropincini@gmail.com>
2022-01-23 20:17:30 -0300

    Move emacs from systemctl to daemon

--- tag-global/emacs.d/init.el
+++ tag-global/emacs.d/init.el
@@ -677,16 +677,3 @@ If neither, we use the current indent-tabs-mode (spaces)."
   (load-theme 'dracula t))
 ;; end of packages - load emacs packages
 ;; end of init.el
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
