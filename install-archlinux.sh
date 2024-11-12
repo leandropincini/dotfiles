03ad17e59e2a6b0c331028c63b996d22cca5ece6
Leandro Pincini <leandropincini@gmail.com>
2024-11-11 22:18:33 -0300

    fix rcup files

--- install-archlinux.sh
+++ install-archlinux.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 env RCRC=/dev/null
-rcup -t archlinux -t global -x README.md -x docs -x home -x install-old.sh -x install-archlinux.sh -x install-macos.sh -x scripts -x toolbox -x .git -x .gitconfig -x .editorconfig -B valholl -q
+rcup -t archlinux \
+     -t global \
+     -x README.md \
+     -x docs \
+     -x home \
+     -x apply.sh \
+     -x install-old.sh \
+     -x install-archlinux.sh \
+     -x install-macos.sh \
+     -x scripts \
+     -x toolbox \
+     -x flake.lock \
+     -x flake.nix \
+     -x .git \
+     -x .gitconfig \
+     -x .editorconfig \
+     -B valholl -q
+
 if [ -f ~/.config/zsh/.zshrc ]; then
     rm ~/.config/zsh/.zshrc
 fi
--- install-macos.sh
+++ install-macos.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 env RCRC=/dev/null
-rcup -t macos -t global -x README.md -x docs -x home -x install-old.sh -x install-archlinux.sh -x install-macos.sh -x scripts -x toolbox -x .git -x .gitconfig -x .editorconfig -B tinydancer -q
+rcup -t macos \
+     -t global \
+     -x README.md \
+     -x docs \
+     -x home \
+     -x apply.sh \
+     -x install-old.sh \
+     -x install-archlinux.sh \
+     -x install-macos.sh \
+     -x scripts \
+     -x toolbox \
+     -x flake.lock \
+     -x flake.nix \
+     -x .git \
+     -x .gitconfig \
+     -x .editorconfig \
+     -B tinydancer -q
+
+if [ -f ~/.config/zsh/.zshrc ]; then
+    rm ~/.config/zsh/.zshrc
+fi
+ln -sv ~/.config/zsh/zshrc ~/.config/zsh/.zshrc
